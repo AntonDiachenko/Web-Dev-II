d61ab019dd55c83e0c5b435a65db5692b278e966
AntonDiachenko <99092120+AntonDiachenko@users.noreply.github.com>
2022-09-08 18:08:50 -0400

    crud-app2-Part2

--- crud-app2/server/index.js
+++ crud-app2/server/index.js
@@ -1,11 +1,45 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 const app = express();
+const mysql = require("mysql2");
 
-
-
-app.get("/", (req, res)=> {
-    res.send("hello world");
+const db = mysql.createPool({
+    host: "localhost",
+    user: "root",
+    password: "root2022root2022",
+    database: "webdev2crud",
 });
+
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/api/get", (req, res)=> {
+    const sqlSelect = "SELECT * FROM movie_reviews;";
+    db.query(sqlSelect, (err, result) => {
+        res.send(result);
+    });
+});
+
+app.post("/api/insert", (req, res)=> {
+    const movieName = req.body.movieName;
+    const movieReview = req.body.movieReview;
+    const sqlInsert = "INSERT INTO movie_reviews (movieName, movieReview) VALUES (?,?)"
+    db.query(sqlInsert, [movieName, movieReview], (err, result)=> {
+        console.log(result);
+    });
+});
+
+
+
+// app.get("/", (req, res)=> {
+
+    // const sqlInsert = "INSERT INTO movie_reviews (movieName, movieReview) VALUES ('newmovie', 'boring');";
+    // db.query(sqlInsert, (err, result)=>{
+    //     res.send("hello newworld");
+    // });
+// });
 
 
 app.listen(3001, () => {
