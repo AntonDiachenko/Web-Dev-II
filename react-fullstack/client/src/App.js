897f356cd467d812ef977a3e68638f63e83fa34d
AntonDiachenko <99092120+AntonDiachenko@users.noreply.github.com>
2022-09-12 14:44:31 -0400

    Episode8

--- react-fullstack/client/src/App.js
+++ react-fullstack/client/src/App.js
@@ -4,6 +4,8 @@ import {BrowserRouter as Router, Route, Link, Routes} from "react-router-dom";
 import Home from "./pages/Home";
 import CreatePost from "./pages/CreatePost";
 import Post from "./pages/Post";
+import Login from "./pages/Login";
+import Registration from "./pages/Registration";
 
 function App() {
   return (
@@ -11,12 +13,16 @@ function App() {
       <Router>
         <div className="navbar">
           <Link to="/">Home Page</Link>    
-          <Link to="/createpost">Create a Post</Link>              
+          <Link to="/createpost">Create a Post</Link>             
+          <Link to="/login">Login</Link> 
+          <Link to="/registration">Registration</Link> 
         </div>
         <Routes>
           <Route path="/" element={<Home />}></Route>
           <Route path="/createpost" element={<CreatePost />}></Route>
           <Route path="/post/:id" element={<Post />}></Route>
+          <Route path="/registration" element={<Registration />}></Route>
+          <Route path="/login" element={<Login />}></Route>
         </Routes>
       </Router>
     </div>
